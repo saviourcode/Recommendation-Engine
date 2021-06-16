74bd5b0879914bcfdec9d83c9c344dcac578009e
Dhruv Tuteja <tutejadhruv@gmail.com>
2021-06-15 19:26:56 -0500

    Update Flask File.py

--- Flask File.py
+++ Flask File.py
@@ -1,24 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+from flask import Flask, render_template
+from flask import render_template_string
 
 import json
 import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-# from Recommender import RecommenderDS
-
-
-# In[ ]:
-
-
 from App import Recommender
-
-
-# In[ ]:
-
 
 app = Flask(__name__)
 CORS(app)
@@ -30,29 +17,30 @@ def recommend():
     result = Recommender().get_recommendation(title)
     print(list(result))
     result_dict = {'Recommended': list(result)}
-    return jsonify(result_dict)
+    #return jsonify(result_dict)
+    return render_template_string('''
+    <table>
+            <tr>
+                <td> Title </td> 
+            </tr>
+
+    {% for title in result.items() %}
+
+            <tr>
+                <td>{{ title }}</td> 
+            </tr>
+
+    {% endfor %}
+
+
+    </table>
+''', result=result)
 
 @app.route("/",methods=['GET'])
 def default():
-    print('Entering Default')
-    return "<h1> Welcome to Netflix Recommendation Engine <h1>"
-
-
-# In[ ]:
+   return render_template('index.html')
 
 
 if __name__ == "__main__":
     app.run() 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
